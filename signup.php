3c001a4a6a73eb8367961f0f16bfe54079bbc1f6
unknown <jp4dr@virginia.edu>
2019-11-14 13:54:24 -0500

    adding signup backend

--- signup.php
+++ signup.php
@@ -1,38 +1,38 @@
 <?php
 session_start();
 
-$db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
- port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
+$db_connection = pg_connect("host=ec2-107-21-125-211.compute-1.amazonaws.com
+ port=5432 dbname=dklt353ih2mrc user=osduvaztmivaka password=b4846020edb6da15efb7cc45ee5f45127c761cc4884a8479ec6ae8d7a2b5a138
 ");
-$username = $_POST['username'];
+//$username = $_POST['username'];
 $email = $_POST['email'];
 $address = $_POST['address'];
-$password = $_POST['password'];
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+//$password = $_POST['password'];
+//$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $city = $_POST['city'];
 $zip = $_POST['zip'];
 $state = $_POST['state'];
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 
-$query = "SELECT* FROM site_users WHERE username='$username' OR email='$email'";
-$result = pg_query($db_connection,$query);
-$user = pg_fetch_assoc($result);
+//$query = "SELECT* FROM customers WHERE username='$username' OR email='$email'";
+//$result = pg_query($db_connection,$query);
+//$user = pg_fetch_assoc($result);
 
 
-if(!$user) {
-    $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$email','$address','$city','$state','$zip','$username','$hashed_password')";
+//if(!$user) {
+    $query_1 = "INSERT INTO customers VALUES('$first_name','$last_name','$email','$address','$city','$state','$zip')";
     $result_1 = pg_query($db_connection,$query_1);
     
-    $_SESSION['username'] = $username;
-    $_SESSION['email'] = $email;
-    $_SESSION['logged_in'] = "logged_in";
+    //$_SESSION['username'] = $username;
+    //$_SESSION['email'] = $email;
+    //$_SESSION['logged_in'] = "logged_in";
     
     //change to homepage for members
-    header('Location: Member_Home_Page.php');
-}else{
-    header('Location: user_exists.html');
-}
+    header('demo.html');
+//}else{
+  //  header('Location: user_exists.html');
+//}
 
 
 
